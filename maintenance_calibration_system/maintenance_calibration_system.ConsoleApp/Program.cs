985ebad5c840fd75089b9d771c8f2b206f8b5cb3
Rafa-coder-byte <rafaelolivareyes.321.xxx@gmail.com>
2024-11-26 23:50:14 +0300

    Creando rama development y proyecto de tipo biblioteca de clases y otro de aplicación de consola

---
+++ maintenance_calibration_system/maintenance_calibration_system.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace maintenance_calibration_system
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ maintenance_calibration_system/maintenance_calibration_system.Domain/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace maintenance_calibration_system.Domain
+{
+    public class Class1
+    {
+
+    }
+}
