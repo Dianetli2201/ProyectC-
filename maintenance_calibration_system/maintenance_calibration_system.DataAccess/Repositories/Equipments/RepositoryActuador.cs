a2b914d1fac0fe724102e94214f8fee15f763953
Dianetli2201 <dianetli2201@gmail.com>
2024-12-08 22:15:45 -0500

    Arreglando clase RepositoryActuador

--- maintenance_calibration_system/maintenance_calibration_system.DataAccess/Repositories/Equipments/RepositoryActuador.cs
+++ maintenance_calibration_system/maintenance_calibration_system.DataAccess/Repositories/Equipments/RepositoryActuador.cs
@@ -3,15 +3,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using maintenance_calibration_system.Domain.Datos_de_Configuración; // Para Actuador y IActuadorRepository
+using maintenance_calibration_system.Domain.Datos_de_Configuración; // Para Actuador
 using maintenance_calibration_system.DataAccess.Contexts; // Para ApplicationContext
-using maintenance_calibration_system.Domain.Types;
 using maintenance_calibration_system.Domain.Common;
-using maintenance_calibration_system.Domain.ValueObjects;
 
-namespace maintenance_calibration_system.Domain.Datos_de_Configuracion
+namespace maintenance_calibration_system.Infrastructure.Repositories
 {
-     public interface IActuadorRepository
+    public interface IActuadorRepository
     {
         void Add(Actuador actuador);
         Actuador GetById(Guid id);
@@ -19,45 +17,15 @@ namespace maintenance_calibration_system.Domain.Datos_de_Configuracion
         void Update(Actuador actuador);
         void Delete(Guid id);
     }
-    public class ActuadorRepository : IActuadorRepository
+
+    public class ActuadorRepository : EquipmentRepository<Actuador>, IActuadorRepository
     {
-        private readonly ApplicationContext _context;
-
-        public ActuadorRepository(ApplicationContext context)
+        public ActuadorRepository(ApplicationContext context) : base(context)
         {
-            _context = context;
+            // Llama al constructor de EquipmentRepository
         }
 
-        public void Add(Actuador actuador)
-        {
-            _context.Set<Actuador>().Add(actuador);
-            _context.SaveChanges();
-        }
-
-        public Actuador GetById(Guid id)
-        {
-            return _context.Set<Actuador>().Find(id);
-        }
-
-        public IEnumerable<Actuador> GetAll()
-        {
-            return _context.Set<Actuador>().ToList();
-        }
-
-        public void Update(Actuador actuador)
-        {
-            _context.Set<Actuador>().Update(actuador);
-            _context.SaveChanges();
-        }
-
-        public void Delete(Guid id)
-        {
-            var actuador = GetById(id);
-            if (actuador != null)
-            {
-                _context.Set<Actuador>().Remove(actuador);
-                _context.SaveChanges();
-            }
-        }
+        // Los métodos CRUD son heredados de EquipmentRepository,
+        // así que no es necesario volver a implementarlos.
     }
 }
