ee39f6b7e8b1ff627f52f79e8839f25ba430e0f7
Dianetli2201 <dianetli2201@gmail.com>
2024-12-10 17:57:30 -0500

    Cambio en el repositorio base

--- maintenance_calibration_system/maintenance_calibration_system.DataAccess/Repositories/Common/RepositoryBase.cs
+++ maintenance_calibration_system/maintenance_calibration_system.DataAccess/Repositories/Common/RepositoryBase.cs
@@ -1,7 +1,47 @@
 using maintenance_calibration_system.DataAccess.Contexts;
 
+/// <summary>
+/// Interfaz genérica para un repositorio base que define las operaciones básicas
+/// de acceso a datos para cualquier tipo de entidad.
+/// </summary>
+/// <typeparam name="T">El tipo de entidad que el repositorio manejará.</typeparam>
+public interface IRepositoryBase<T> where T : class
+{
+    /// <summary>
+    /// Añade una nueva entidad al repositorio.
+    /// </summary>
+    /// <param name="entity">La entidad a añadir.</param>
+    void Add(T entity);
+
+    /// <summary>
+    /// Busca una entidad por su identificador único.
+    /// </summary>
+    /// <param name="id">El identificador único de la entidad.</param>
+    /// <returns>La entidad correspondiente al identificador, o null si no se encuentra.</returns>
+    T GetById(Guid id);
+
+    /// <summary>
+    /// Devuelve todas las entidades del tipo especificado.
+    /// </summary>
+    /// <returns>Una colección de todas las entidades.</returns>
+    IEnumerable<T> GetAll();
+
+    /// <summary>
+    /// Actualiza una entidad existente en el repositorio.
+    /// </summary>
+    /// <param name="entity">La entidad a actualizar.</param>
+    void Update(T entity);
+
+    /// <summary>
+    /// Elimina una entidad del repositorio por su identificador único.
+    /// </summary>
+    /// <param name="id">El identificador único de la entidad a eliminar.</param>
+    void Delete(Guid id);
+}
+
+
 /// <summary>Utiliza genéricos para permitir que cualquier tipo de entidad (que sea una clase) pueda ser manejada por el repositorio.</summary>
-public abstract class RepositoryBase<T> where T : class
+public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
 {
     protected readonly ApplicationContext _context;
 
