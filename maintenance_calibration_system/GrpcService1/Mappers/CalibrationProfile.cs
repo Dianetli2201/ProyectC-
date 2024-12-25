254879981395df361fe5e85ce4640130cbc6f9a8
Dianetli2201 <dianetli2201@gmail.com>
2024-12-25 15:30:14 -0500

    Creacion del servicio de calibarcion

---
+++ maintenance_calibration_system/GrpcService1/Mappers/CalibrationProfile.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using maintenance_calibration_system.Domain.Datos_Historicos;
+using maintenance_calibration_system.GrpcProtos;
+using System.Collections.Generic;
+
+namespace GrpcService1.Mappers
+{
+    public class CalibrationProfile : Profile
+    {
+        public CalibrationProfile()
+        {
+            CreateMap<Calibration, CalibrationDTO>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id.ToString())) // Convertir Guid a string
+                .ForMember(dest => dest.CalibratedSensors, opt => opt.MapFrom(src =>
+                    src.CalibratedSensors.ConvertAll(sensor => new SensorDTO
+                    {
+                        Id = sensor.Id.ToString(), // Asumiendo que Sensor tiene una propiedad Id de tipo Guid
+                        Name = sensor.Name // Asumiendo que Sensor tiene una propiedad Name
+                    })));
+
+            CreateMap<CalibrationDTO, Calibration>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => Guid.Parse(src.Id))) // Convertir string a Guid
+                .ForMember(dest => dest.CalibratedSensors, opt => opt.MapFrom(src =>
+                    src.CalibratedSensors.ConvertAll(sensorDto => new Sensor
+                    {
+                        Id = Guid.Parse(sensorDto.Id), // Asumiendo que Sensor tiene una propiedad Id de tipo Guid
+                        Name = sensorDto.Name // Asumiendo que Sensor tiene una propiedad Name
+                    })));
+        }
+    }
+}
